ce63a1fc3123d147a42ea28898ab82a41492e5a5
panatyuki <admiral.dancho@gmail.com>
2023-12-21 12:27:29 +0700

    Add a new code for run Buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greeting, setGreeting] = useState("Greeting Message");
+
+  const handleButtonClick = (message) => {
+    setGreeting(message);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greeting}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => handleButtonClick("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => handleButtonClick("Hi!")}>Hi!</button>
+        <button onClick={() => handleButtonClick("你好!")}>你好!</button>
+        {/* ลองเพิ่มคำทักทายภาษาญี่ปุ่นเข้าไปครับ */}
+        <button onClick={() => handleButtonClick("おはようございます！")}>
+          おはようございます！
+        </button>
       </div>
     </div>
   );
